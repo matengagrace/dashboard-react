c3734c88a65a4c44a62f01f7f0bc161fec8dae07
matengagrace <matengagraceandre@gmail.com>
2026-05-07 11:06:16 +0200

    mise a jour de partie message

--- src/components/ModaleEdit.jsx
+++ src/components/ModaleEdit.jsx
@@ -6,7 +6,7 @@ function ModaleEdit({isOpen,onClose}) {
         <div class="fixed inset-0 z-50 grid place-content-center bg-black/50 p-4" role="dialog" aria-modal="true" aria-labelledby="modalTitle">
   <div class="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
     <div class="flex items-start justify-between">
-      <h2 id="modalTitle" class="text-xl font-bold bg-blue-600 p-1 rounded-lg w-full text-gray-100 sm:text-2xl">Modifier Utilisateur</h2>
+      <h2 id="modalTitle" class="text-xl font-bold bg-blue-600 p-1 rounded-lg w-full text-gray-100 sm:text-2xl">Modifier l'Utilisateur</h2>
 
       <button onClick={onClose} type="button" class="-me-4 -mt-4 rounded-full p-2 text-gray-400 transition-colors hover:bg-gray-50 hover:text-gray-600 focus:outline-none" aria-label="Close">
         <svg xmlns="http://www.w3.org/2000/svg" class="size-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -21,13 +21,13 @@ function ModaleEdit({isOpen,onClose}) {
         <span class="text-sm font-medium text-gray-700">
           Name:
         </span>
-        <input type="text" id="Confirm" class="mt-0.5 w-full h-[40px] rounded border border-gray-400 outline-0 shadow-sm sm:text-sm"/>
+        <input type="text" id="Confirm" class="mt-0.5 w-full pl-2 h-[40px] rounded border border-gray-400 outline-0 shadow-sm sm:text-sm"/>
       </label>
       <label for="Confirm" class="mt-4 block w-[400px]">
         <span class="text-sm font-medium text-gray-700">
           Role:
         </span>
-        <select name="" id="" class="mt-0.5 w-full h-[40px] rounded border border-gray-400 outline-0 shadow-sm sm:text-sm">
+        <select name="" id="" class="mt-0.5 w-full h-[40px] rounded pl-2 border border-gray-400 outline-0 shadow-sm sm:text-sm">
             <option value="#">--------choisir role---------</option>
             <option value="Vampire Warrior">Vampire Warrior</option>
             <option value="Vampire Gentleman">Vampire Gentleman</option>
@@ -39,7 +39,7 @@ function ModaleEdit({isOpen,onClose}) {
         <span class="text-sm font-medium text-gray-700">
           Status:
         </span>
-        <select name="" id="" class="mt-0.5 w-full h-[40px] rounded border border-gray-400 outline-0 shadow-sm sm:text-sm">
+        <select name="" id="" class="mt-0.5 w-full h-[40px] rounded border pl-2 border-gray-400 outline-0 shadow-sm sm:text-sm">
             <option value="#">--------choisir status---------</option>
             <option value="Active">Active</option>
             <option value="Inactive">Inactive</option>
@@ -49,7 +49,7 @@ function ModaleEdit({isOpen,onClose}) {
         <span class="text-sm font-medium text-gray-700">
           DoB:
         </span>
-        <input type="date" id="Confirm" class="mt-0.5 w-full h-[40px] rounded border border-gray-400 outline-0 shadow-sm sm:text-sm"/>
+        <input type="date" id="Confirm" class="mt-0.5 w-full h-[40px] pl-2 rounded border border-gray-400 outline-0 shadow-sm sm:text-sm"/>
       </label>
 
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,13 @@ import { Bell, Mail, Search, CheckCheck } from "lucide-react";
 import User from "../assets/user-avatar.png";
 import User2 from "../assets/avatar.png";
 import Profil from "./Profil";
+  import {View, Eye} from "lucide-react";
 
 function Navbar() {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
   const [openProfil, setOpenProfil] = useState(false);
   const [openNoti,setOpenNoti] =useState(false)
+  const [openMsg,setOpenMsg] =useState(false)
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -35,11 +37,95 @@ function Navbar() {
       </div>
 
       <div className="ml-auto flex items-center gap-4">
-        <div className="bg-white p-1.5 rounded-full font-bold text-[18px] cursor-pointer relative rounded-box">
-          <Mail className="hover:text-blue-500" />
+        <div className="bg-white p-1.5 rounded-full font-bold text-[18px]  relative rounded-box z-3">
+          <Mail onClick={()=>setOpenMsg(!openMsg)} className="hover:text-blue-500 cursor-pointer" />
           <p className="bg-blue-500 px-2 rounded-full text-[14px] text-white absolute -top-2.5 left-[22px]">
                   2
                 </p>
+            {openMsg && (
+            <div className="w-[250px] h-[40vh]  bg-white shadow-lg rounded-lg overflow-hidden flex items-center justify-between p-2 flex-col  absolute top-15 -right-2 border border-gray-300">
+            <div className="border-b-1 w-full text-sm border-gray-400 pb-2">
+              <div className="flex justify-between items-center relative">
+                <h2>Messages</h2>
+                
+                
+              </div>
+            </div>
+            <div className="flex flex-col gap-1">
+              
+              <div className="flex items-center justify-between w-full hover:bg-gray-200 bg-green-100 rounded-lg px-1">
+                <div className="bg-white rounded-full w-9 h-9 font-bold text-[16px] flex items-center justify-center  rounded-box">
+                  <img src={User2} alt="User" className=" rounded-full" />
+                </div>
+                <div className=" opacity-100  z-8">
+                  <div className="ml-2 text-sm">
+                    <h4 className="text-[10px] font-bold">Ange Marie</h4>
+                    <p className="text-[8px] text-gray-400">
+                      Lorem, ipsum dolor sit amet consectetur.
+                    </p>
+                  </div>
+                </div>
+                <div className="text-[9px] w-[50px] flex flex-col items-end">
+                  <p className="text-gray-600">12:22 min</p>
+                  <p className="text-[12px] cursor-pointer"><Eye size={16} /></p>
+                </div>
+              </div>
+              <div className="flex items-center justify-between w-full hover:bg-gray-200 bg-green-100 rounded-lg px-1">
+                <div className="bg-white rounded-full w-9 h-9 font-bold text-[16px] flex items-center justify-center  rounded-box">
+                  <img src={User} alt="User" className=" rounded-full" />
+                </div>
+                <div className=" opacity-100  z-8">
+                  <div className="ml-2 text-sm">
+                    <h4 className="text-[10px] font-bold">Matenga Graace</h4>
+                    <p className="text-[8px] text-gray-400">
+                      Lorem, ipsum dolor sit amet consectetur.
+                    </p>
+                  </div>
+                </div>
+                <div className="text-[9px] w-[50px] flex flex-col items-end">
+                  <p className="text-gray-600">10:12 min</p>
+                  <p className="text-[12px] cursor-pointer"><Eye size={16} /></p>
+                </div>
+              </div>
+              <div className="flex items-center justify-between w-full hover:bg-gray-200 rounded-lg px-1">
+                <div className="bg-white rounded-full w-9 h-9 font-bold text-[16px] flex items-center justify-center  rounded-box">
+                  <img src={User2} alt="User" className=" rounded-full" />
+                </div>
+                <div className=" opacity-100  z-8">
+                  <div className="ml-2 text-sm">
+                    <h4 className="text-[10px] font-bold">Marie Ange</h4>
+                    <p className="text-[8px] text-gray-400">
+                      Lorem, ipsum dolor sit amet consectetur.
+                    </p>
+                  </div>
+                </div>
+                <div className="text-[9px] w-[50px] flex flex-col items-end">
+                  <p className="text-gray-600">11:10 min</p>
+                  <p className="text-[12px] cursor-pointer text-blue-500"><Eye size={16} /></p>
+                </div>
+              </div>
+              <div className="flex items-center justify-between w-full hover:bg-gray-200 rounded-lg px-1">
+                <div className="bg-white rounded-full w-9 h-9 font-bold text-[16px] flex items-center justify-center  rounded-box">
+                  <img src={User} alt="User" className=" rounded-full" />
+                </div>
+                <div className=" opacity-100  z-8">
+                  <div className="ml-2 text-sm">
+                    <h4 className="text-[10px] font-bold">Matenga Graace</h4>
+                    <p className="text-[8px] text-gray-400">
+                      Lorem, ipsum dolor sit amet consectetur.
+                    </p>
+                  </div>
+                </div>
+                <div className="text-[9px] w-[50px] flex flex-col items-end">
+                  <p className="text-gray-600">12:22 min</p>
+                  <p className="text-[12px] cursor-pointer text-blue-500"><Eye size={16} /></p>
+                </div>
+              </div>
+            </div>
+            <div className="text-[13px] font-semibold text-gray-700 border-t-1 border-gray-400 w-full text-center cursor-pointer hover:text-blue-700 flex items-center justify-center gap-2">
+              <View size={16} /> <p>Voir plus</p></div>
+          </div>
+          )}
         </div>
         <div onClick={()=> setOpenNoti(!openNoti)} className="bg-white p-1.5 rounded-full font-bold text-[18px]  rounded-box relative z-8">
           <Bell className="hover:text-rose-500 cursor-pointer" />
@@ -52,7 +138,7 @@ function Navbar() {
             <div className="border-b-1 w-full text-sm border-gray-400 pb-2">
               <div className="flex justify-between items-center relative">
                 <h2>Notification</h2>
-                <p className="bg-blue-500 px-1 rounded-full text-[14px] text-white absolute top-0 left-[84px]">
+                <p className="bg-red-500 px-1 rounded-full text-[14px] text-white absolute top-0 left-[84px]">
                   3
                 </p>
                 <p className="text-[12px] flex items-center text-blue-500 hover:text-blue-800 gap-1">
@@ -148,7 +234,9 @@ function Navbar() {
                 </div>
               </div>
             </div>
-            <div className="text-[13px] font-semibold text-gray-700 border-t-1 border-gray-400 w-full text-center cursor-pointer hover:text-blue-700">Voir plus</div>
+            <div className="text-[13px] font-semibold text-gray-700 border-t-1 border-gray-400 w-full text-center cursor-pointer hover:text-blue-700 flex items-center justify-center gap-2">
+              <View size={16}/> <p>Voir plus</p>
+            </div>
           </div>
           )}
         </div>
